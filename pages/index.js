59d389c072a5aa78813376d4d698b1f0fa26e0e1
zemeteam <76970969+zemeteam@users.noreply.github.com>
2021-03-22 10:18:42 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Zeme team
+          ZEME TEAM
         </h1>
         <b>Earn shielded Zcash for creating fun and educational zemes.</b>
       </main>
