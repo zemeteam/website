667d11114fa6a70fb66149cd90015f65cd280fbb
zemeteam <zigazcash@gmail.com>
2021-07-09 23:01:42 -0700

    create optimization

--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -87,13 +87,7 @@ export default async(req, res) => {
                     // upload image to cloudinary (create additional sizes needed)
                     const image = await Cloudinary.uploader.upload(post.files.image.path, { 
                         public_id: slug, 
-                        moderation: 'aws_rek',
-                        eager: [
-                            { crop: 'scale', width: 700 },
-                            { format: 'webp', crop: 'scale' },
-                            { format: 'webp', crop: 'scale', width: 700 },
-                            { format: 'webp', crop: 'scale', width: 1200 }
-                        ] 
+                        moderation: 'aws_rek'
                     })
 
                     // ensure the image ratio is within our accepted boundaries 
@@ -115,6 +109,14 @@ export default async(req, res) => {
                                 }
                             )
                             if (data) {
+                                // run the transformations
+                                Cloudinary.image(slug, {transformation: [
+                                    { crop: 'scale', width: 700 },
+                                    { format: 'webp', crop: 'scale' },
+                                    { format: 'webp', crop: 'scale', width: 700 },
+                                    { format: 'webp', crop: 'scale', width: 1200 }
+                                ]})
+
                                 res.status(200).json({ message: `Succesfully created post with slug ${slug}.`, slug: slug })
                             }
             
