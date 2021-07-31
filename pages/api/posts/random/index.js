b6b4b7ac2faa1d553b453fe8c372c7c7be87e24f
zemeteam <zigazcash@gmail.com>
2021-07-30 21:01:12 -0700

    lower page count

--- pages/api/posts/random/index.js
+++ pages/api/posts/random/index.js
@@ -2,7 +2,7 @@ import { Supabase } from '../../../../lib/supabase'
 import _ from 'underscore'
 
 const POST_STATUS_LIVE = 1
-const POSTS_PER_PAGE = 500
+const POSTS_PER_PAGE = 150
 const POST_VIEW_THRESHOLD = 5
 
 export default async(req, res) => {
